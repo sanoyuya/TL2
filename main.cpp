9ea686dc9dcd8952dea061420cf90af159739c3d
sano_yuya <k021g1256@g.neec.ac.jp>
2023-10-02 12:21:22 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 #include<cstdlib>
 #include<cstdint>
 
-int32_t main()
+int32_t main(int32_t argc,char* argv[])
 {
-	printf("Hello,World!\n");
+	//argv‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int32_t i = 0; i < argc; i++)
+	{
+		//•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
